a6c9c22d368e6b3ea8d861144620cc705b50ba8f
Rafael Alberola <alberolarafa0@gmail.com>
2022-09-15 20:24:08 +0200

    Update

---
+++ assets/js/tailwind.config.js
@@ -0,0 +1,100 @@
+// NOTE: AS REQUESTED, TAILWIND CSS 3.0 HAS FULL SUPPORT FOR ALL COLORS BY DEFAULT
+// SEE https://tailwindcss.com/blog/tailwindcss-v3#every-color-out-of-the-box.
+
+// As of 2020-12-22, this tailwind.config.js file includes all colors
+// from the Tailwind CSS color palette reference laid out in full at this
+// link: https://tailwindcss.com/docs/customizing-colors#color-palette-reference.
+// Note that this will increase your unpurged Tailwind CSS file size immensely,
+// for me it went from 3.64MB to 7.62MB! Be sure to purge your CSS in production!
+
+// First we import the tailwindcss/colors dependency, as referenced
+// here: https://tailwindcss.com/docs/customizing-colors#curating-colors
+
+// tailwind.config.js
+module.exports = {
+    darkMode: 'class',
+    // ...
+}
+
+var themeToggleDarkIcon = document.getElementById('theme-toggle-dark-icon');
+var themeToggleLightIcon = document.getElementById('theme-toggle-light-icon');
+
+// Change the icons inside the button based on previous settings
+if (localStorage.getItem('color-theme') === 'dark' || (!('color-theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    themeToggleLightIcon.classList.remove('hidden');
+} else {
+    themeToggleDarkIcon.classList.remove('hidden');
+}
+
+var themeToggleBtn = document.getElementById('theme-toggle');
+
+themeToggleBtn.addEventListener('click', function() {
+
+    // toggle icons inside button
+    themeToggleDarkIcon.classList.toggle('hidden');
+    themeToggleLightIcon.classList.toggle('hidden');
+
+    // if set via local storage previously
+    if (localStorage.getItem('color-theme')) {
+        if (localStorage.getItem('color-theme') === 'light') {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('color-theme', 'dark');
+        } else {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('color-theme', 'light');
+        }
+
+        // if NOT set via local storage previously
+    } else {
+        if (document.documentElement.classList.contains('dark')) {
+            document.documentElement.classList.remove('dark');
+            localStorage.setItem('color-theme', 'light');
+        } else {
+            document.documentElement.classList.add('dark');
+            localStorage.setItem('color-theme', 'dark');
+        }
+    }
+
+});
+
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+    purge: ["./**/*.html", "./**/*.js"],
+    darkMode: false,
+    theme: {
+        extend: {},
+        colors: {
+            // Now we build the full color palette, using all colors available
+            // as shown at this link: https://tailwindcss.com/docs/customizing-colors#color-palette-reference
+            transparent: "transparent",
+            current: "currentColor",
+            black: "#000",
+            white: "#fff",
+            bluegray: colors.blueGray,
+            coolgray: colors.coolGray,
+            gray: colors.gray,
+            truegray: colors.trueGray,
+            warmgray: colors.warmGray,
+            red: colors.red,
+            orange: colors.orange,
+            amber: colors.amber,
+            yellow: colors.yellow,
+            lime: colors.lime,
+            green: colors.green,
+            emerald: colors.emerald,
+            teal: colors.teal,
+            cyan: colors.cyan,
+            sky: colors.sky,
+            blue: colors.blue,
+            indigo: colors.indigo,
+            violet: colors.violet,
+            purple: colors.purple,
+            fuchsia: colors.fuchsia,
+            pink: colors.pink,
+            rose: colors.rose,
+        },
+    },
+    variants: {},
+    plugins: [],
+};
